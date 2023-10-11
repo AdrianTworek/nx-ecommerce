eb09d48ed39c688cfaabf2c9b501809252e5fd8b
Adrian Tworek <adrian.tworek.it@gmail.com>
2023-10-11 21:48:27 +0200

    feat: test healthcheck route on web

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   standalone: true,
   imports: [RouterModule],
+  providers: [HttpClient],
   selector: 'nx-ecommerce-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'web';
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
+  result = '';
+
+  ngOnInit(): void {
+    this.getApi();
+  }
+
+  getApi() {
+    return this.http.get<{ message: string }>('/api').subscribe((response) => {
+      this.result = response.message;
+    });
+  }
 }
