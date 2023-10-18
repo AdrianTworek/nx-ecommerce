cf34a03c5d2e0a458babfed30ccac2428d8bbef7
Adrian Tworek <adrian.tworek.it@gmail.com>
2023-10-18 13:44:01 +0200

    refactor: App component cleanup

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -1,26 +1,10 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import { HlmButtonModule } from '@spartan-ng/ui-button-helm';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, HlmButtonModule],
-  providers: [HttpClient],
+  imports: [RouterModule],
   selector: 'nx-ecommerce-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
-  http = inject(HttpClient);
-  result = '';
-
-  ngOnInit(): void {
-    this.getApi();
-  }
-
-  getApi() {
-    return this.http.get<{ message: string }>('/api').subscribe((response) => {
-      this.result = response.message;
-    });
-  }
-}
+export class AppComponent {}
