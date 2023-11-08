75e31259c51752b1d723fcb548e87c6b7d2cc6b7
Adrian Tworek <adrian.tworek.it@gmail.com>
2023-11-08 21:41:29 +0100

    feat: prepare ui for auth forms

--- libs/web/feature-auth/src/lib/feature-auth/feature-auth.component.ts
+++ libs/web/feature-auth/src/lib/feature-auth/feature-auth.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
 
 @Component({
   selector: 'nx-ecommerce-feature-auth',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    HlmInputDirective,
+    HlmButtonDirective,
+    HlmLabelDirective,
+  ],
   templateUrl: './feature-auth.component.html',
 })
-export class FeatureAuthComponent {}
+export class FeatureAuthComponent {
+  isSignup = signal(true);
+
+  toggleForm() {
+    this.isSignup.update(() => !this.isSignup());
+  }
+}
